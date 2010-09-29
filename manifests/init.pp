588d6aac3d82661a216ca38899889f3f239d1d91
Sean Porter <portertech@gmail.com>
2010-09-29 11:11:41 -0700

    first commit

---
+++ manifests/init.pp
@@ -0,0 +1,56 @@
+# Class: serverdensity
+#
+# This class installs and configures the Server Density monitoring agent: http://www.serverdensity.com/
+#
+# Notes:
+#  This class is Ubuntu/Debian specific for now.
+#  By Sean Porter, Gastown Labs Inc.
+#
+# Actions:
+#  - adds to the apt repository list
+#  - installs and configures the Server Density monitoring agent, sd-agent
+#
+# Requires:
+#
+# Sample Usage (Monitoring MongoDB):
+#  include serverdensity
+#
+#  serverdensity::setup { "server-density-subdomain":
+#    agent_key => "b82e833n4o9h189a352k8ds67725g3jy",
+#    options => ["mongodb_server: localhost"],
+#  }
+#
+class serverdensity {
+	define setup ( $agent_key, $options=[""] ) {
+		exec { "server-density-apt-key":
+			path => "/bin:/usr/bin",
+			command => "wget http://www.serverdensity.com/downloads/boxedice-public.key -O - | apt-key add -",
+			unless => "apt-key list | grep -i boxedice",
+		}
+
+		exec { "server-density-apt-list":
+			path => "/bin:/usr/bin",
+			command => "echo 'deb http://www.serverdensity.com/downloads/linux/debian lenny main' >> /etc/apt/sources.list;apt-get update",
+			unless => "cat /etc/apt/sources.list | grep -i serverdensity",
+			require => Exec["server-density-apt-key"],
+		}
+
+		package { "sd-agent":
+			ensure => installed,
+			require => Exec["server-density-apt-list"],
+		}
+
+		file { "/etc/sd-agent/config.cfg":
+			content => template("serverdensity/config.cfg.erb"),
+		    mode => "0644",
+			require => Package["sd-agent"],
+		}
+		
+		service { "sd-agent":
+			ensure => running,
+			enable => true,
+			subscribe => Package["sd-agent"],
+			require => File["/etc/sd-agent/config.cfg"],
+		}
+	}
+}
